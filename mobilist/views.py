6ccfe3812b747f09a78992f0a5b9d6d5027f2034
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-02-24 09:40:47 +0100

    commentaire de la class ModificationForm

--- mobilist/views.py
+++ mobilist/views.py
@@ -141,10 +141,19 @@ class ResetPasswordFrom(FlaskForm):
     valider = PasswordField("Confirmer mot de passe")
 
 class ModificationForm(FlaskForm):
+    """
+    Formulaire de modification du mot de passe
+
+    Attributes :
+        nom (StringField) : Champ pour le nom de l'utilisateur
+        prenom (StringField) : Champ pour le prénom de l'utilisateur
+        mdp_actuel (PasswordField) : Champ pour le mot de passe actuel de l'utilisateur
+        mdp (PasswordField) : Champ pour le nouveau mot de passe
+        mdp_confirm (PasswordField) : Champ de confirmation pour le nouveau mot de passe
+    """
     nom = StringField('Votre Nom', validators=[DataRequired()])
     prenom = StringField('Votre Prénom', validators=[DataRequired()])
-    mdp_actuel = PasswordField('Mot de passe actuel',
-                               validators=[DataRequired()])
+    mdp_actuel = PasswordField('Mot de passe actuel', validators=[DataRequired()])
     mdp = PasswordField('Nouveau mot de passe', validators=[DataRequired()])
     mdp_confirm = PasswordField('Confirmer le mot de passe', validators=[DataRequired()])
     different = False
