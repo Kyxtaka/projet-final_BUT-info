1bc4ea4499187cdbf242b8e1c0dc4f62e7823401
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-02-25 17:09:57 +0100

    début de l'ajout d'un utilisateur

--- mobilist/routes/admin/admin.py
+++ mobilist/routes/admin/admin.py
@@ -1,27 +1,10 @@
-from ...models.classes.User import User
-from ...models.classes.TypeBien import TypeBien
-from ...models.classes.Proprietaire import Proprietaire
-from ...models.classes.Logement import Piece
-from ...models.classes.Logement import LogementType
-from ...models.classes.Logement import Logement
-from ...models.classes.Justificatif import Justificatif
-from ...models.classes.Categorie import Categorie
-from ...models.classes.Logement import Bien
-from ...models.classes.Logement import AVOIR
-from ...models.classes.Avis import Avis
-import json
 
 from flask import Blueprint
 from flask import (
-    jsonify, 
     render_template, 
-    redirect, 
-    render_template, url_for
+    render_template
     )
-from flask import request
-from flask_login import login_required
-from flask_login import login_user, current_user
-from ...app import db
+
 
 admin_bp = Blueprint('admin', __name__)
 
